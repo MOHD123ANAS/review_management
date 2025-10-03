65bf83af8e97143aea4ecf9f4f0ecff15e66096a
MOHD123ANAS <mohammedanas18025@gmail.com>
2025-10-03 15:11:18 +0530

    1

--- review_management/review_mangement/customizations/item_review.py
+++ review_management/review_mangement/customizations/item_review.py
@@ -4,7 +4,6 @@ def create_item_review_if_not_exists(doc, method=None):
 
     exists = frappe.db.exists("Item Review", {"item": doc.name})
     if not exists:
-        # Create Item Review document
         item_review = frappe.get_doc({
             "doctype": "Item Review",
             "item": doc.name,   
